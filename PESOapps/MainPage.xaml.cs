a269336f642421199b3f3832523a7f788f3a1506
kdahirocmr122315 <kdahirocmr122315@gmail.com>
2025-02-14 16:50:17 +0800

    Add project files.

---
+++ PESOapps/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PESOapps
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PESOapps/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using PESOapps.Shared.Services;
+
+namespace PESOapps.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return DeviceInfo.Idiom.ToString();
+        }
+
+        public string GetPlatform()
+        {
+            return DeviceInfo.Platform.ToString() + " - " + DeviceInfo.VersionString;
+        }
+    }
+}

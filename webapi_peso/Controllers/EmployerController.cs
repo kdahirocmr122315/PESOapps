96fdf0d11b4a5dd8a8f284a5fdded6cf266f6a91
MIS <MIS@KIOSK01>
2025-10-01 14:55:10 +0800

    GetInterViewedApplicants

--- webapi_peso/Controllers/EmployerController.cs
+++ webapi_peso/Controllers/EmployerController.cs
@@ -693,5 +693,21 @@ namespace webapi_peso.Controllers
             }
         }
 
+        [HttpGet("GetInterViewedApplicants/{Id}")]
+        public IActionResult GetInterViewedApplicants(string Id)
+        {
+            using var db = dbFactory.CreateDbContext();
+            var list = db.EmployerInterviewedApplicants.Where(x => x.EmployerId == Id).OrderByDescending(x => x.DateInterviewed);
+            var result = new List<ApplicantInformation>();
+            foreach (var i in list)
+            {
+                var a = db.ApplicantInformation.Where(x => x.AccountId == i.ApplicantAccountId).OrderByDescending(x => x.DateLastUpdate).MyDistinctBy(x => x.AccountId).FirstOrDefault();
+                var IsHired = db.EmployerHiredApplicants.Any(x => x.ApplicantAccountId == i.ApplicantAccountId && x.EmployerId == Id);
+                if (a != null && !IsHired)
+                    result.Add(a);
+            }
+            return Ok(result);
+        }
+
     }
 }
